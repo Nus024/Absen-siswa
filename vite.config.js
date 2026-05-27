76f40805cc8b50809e3b020a6191ce43c7ac1bb6
nus024 <yunusassyaif24.com>
2026-05-27 18:52:02 +0700

    fix vite build config

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          qr: ['html5-qrcode', 'qrcode'],
-          xlsx: ['xlsx'],
-        }
-      }
-    }
-  }
 })
